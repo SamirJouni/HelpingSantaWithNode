6521ed1b51a76b3010f91b8a2e2f82932a449da9
F1r3byte <f1r3byte@yandex.com>
2018-04-18 00:05:18 +0200

    fixed bug with count

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ fs.readFile('./input.txt', (error, data) =>{
   console.time('funchallenge');
 
   var directions = data.toString();
-  for (var up = -1, index = 0; index != -1; up++, index = directions.indexOf('(', index + 1));
-  for (var down = -1, index = 0; index != -1; down++, index = directions.indexOf(')', index + 1));
+  for (var up = -1, index = -2; index != -1; up++, index = directions.indexOf('(', index + 1));
+  for (var down = -1, index = -2; index != -1; down++, index = directions.indexOf(')', index + 1));
 
   console.timeEnd('funchallenge');
   console.log('Santa, go to floor:', up - down);
