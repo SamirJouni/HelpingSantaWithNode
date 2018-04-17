329d52af3a422f9b4d6b15aa0b23d499a3e710fe
F1r3byte <f1r3byte@yandex.com>
2018-04-17 23:59:42 +0200

    added time measurement

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ fs.readFile('./input.txt', (error, data) =>{
   if (error){
     console.log(error);
   }
+  console.time('funchallenge');
 
   var directions = data.toString();
   for (var up = -1, index = 0; index != -1; up++, index = directions.indexOf('(', index + 1));
   for (var down = -1, index = 0; index != -1; down++, index = directions.indexOf(')', index + 1));
 
-  console.log(up - down);
+  console.timeEnd('funchallenge');
+  console.log('Santa, go to floor:', up - down);
 })
